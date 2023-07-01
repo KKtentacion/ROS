8f5316b6e976f4895217d556734b725e8cd38921
KKtentacion <535976386@qq.com>
2023-07-02 00:50:44 +0800

    add some markdown in my code

--- catkin_ws/src/rostask_pkg/src/show_color_camera_node.cpp
+++ catkin_ws/src/rostask_pkg/src/show_color_camera_node.cpp
@@ -18,10 +18,14 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "show_color_camera_node");
-  ros::NodeHandle n;
-  image_transport::ImageTransport it(n);
+  ros::init(argc, argv, "show_color_camera_node");  //初始化"show_color_camera_node"这个节点
+  ros::NodeHandle n;  // 声明用于ROS系统和通信的节点句柄
+  image_transport::ImageTransport it(n); 
+  // 声明订阅者，创建一个订阅者image_sub，
+  // 它利用sensor_msgs功能包的的ImageConstPtr&消息文件。
+  // 话题名称是"/camera/color/image_raw"，订阅者队列(queue)的大小设为1000。
   image_transport::Subscriber image_sub_ = it.subscribe("/camera/color/image_raw", 1000,imageCb);
+  // 用于调用后台函数，等待接收消息。在接收到消息时执行后台函数。
   ros::spin();
   return 0;
 }
--- catkin_ws/src/rostask_pkg/src/show_odom_node.cpp
+++ catkin_ws/src/rostask_pkg/src/show_odom_node.cpp
@@ -1,13 +1,8 @@
 #include "show_odom_node.h"
 
-/*
-geometry_msgs/PoseWithCovariance pose
-  geometry_msgs/Pose pose
-    geometry_msgs/Point position
-      float64 x
-      float64 y
-      float64 z
-*/
+// 这是一个消息后台函数，
+// 此函数在收到一个下面设置的名为sensor_msgs的话题时候被调用。 
+// 输入的消息是从sensor_msgs功能包接收Odometry消息。
 
 void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
@@ -20,9 +15,13 @@ void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "show_odom_node");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "show_odom_node");  //初始化"show_imu_node"这个节点
+  ros::NodeHandle n;  // 声明用于ROS系统和通信的节点句柄
+  // 声明订阅者，创建一个订阅者sub，
+  // 它利用sensor_msgs功能包的的Odometry消息文件。
+  // 话题名称是"/odom"，订阅者队列(queue)的大小设为1000。
   ros::Subscriber sub = n.subscribe("/odom", 1000, chatterCallback);
+  // 用于调用后台函数，等待接收消息。在接收到消息时执行后台函数。
   ros::spin();
   return 0;
 }
